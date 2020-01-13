03292c4815d2ec431043adcc6f31b716e924603c
xincxiong <xiongxinland@gmail.com>
2020-01-13 20:11:25 +0800

    add ros installation

---
+++ tools/HiLens Kit + ROS/install_rpm.sh
@@ -0,0 +1,18 @@
+rpm -i python2-2.7.17-1.fc29.aarch64.rpm
+rpm -i --nodeps python2-libs-2.7.17-1.fc29.aarch64.rpm
+rpm -i python2-2.7.17-1.fc29.aarch64.rpm
+rpm -i python2-setuptools-40.8.0-1.fc29.noarch.rpm
+rpm -U python2-pip-18.1-1.fc29.noarch.rpm
+rpm -U python2-netifaces-0.10.6-5.fc29.aarch64.rpm
+rpm -U python2-nose-1.3.7-21.fc29.noarch.rpm
+rpm -U libgfortran-8.2.1-2.fc29.aarch64.rpm
+rpm -U openblas-0.3.7-1.fc29.aarch64.rpm
+rpm -U openblas-serial-0.3.7-1.fc29.aarch64.rpm
+rpm -U openblas-threads-0.3.7-1.fc29.aarch64.rpm
+rpm -U python2-numpy-1.15.1-1.fc29.aarch64.rpm
+rpm -U tmux-2.9a-2.fc29.aarch64.rpm
+rpm -i gpm-libs-1.20.7-16.fc29.aarch64.rpm
+rpm -i vim-filesystem-8.1.2198-1.fc29.noarch.rpm
+rpm -i vim-common-8.1.2198-1.fc29.aarch64.rpm
+rpm -U vim-enhanced-8.1.2198-1.fc29.aarch64.rpm
+rpm -U --reinstall python2-numpy-1.15.1-1.fc29.aarch64.rpm
